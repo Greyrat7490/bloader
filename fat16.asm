211087569355c4011685ef39dde592d6dc910c70
Greyrat7490 <kevinschaperjahn13@gmail.com>
2023-01-10 19:39:13 +0100

    read multiple sectors, check for errors * more waits (important) * retry 4x * check for errors

--- fat16.asm
+++ fat16.asm
@@ -4,91 +4,109 @@
 %define start_cluster 0x1a      ; offset to start_cluster
 %define file_size 0x1c          ; offset to file_size
 
-%define kernel_addr 0x128000
-%define fat_addr 0x120000       ; (0x120000 - 0x128000)
-%define root_dir_addr 0xb000    ; (0xb000 - 0xf000)
+%define root_dir_addr 0x120000  ; (0x120000 - 0x124000)
+%define fat_addr 0x124000       ; (0x124000 - 0x12c000)
+%define kernel_addr 0x130000
 
 [BITS 64]
 ; eax = LBA
-; cl = sector count
+; bl = size in sector (0 -> 256)
 ; rdi = dest addr
-readSectors64:
+readATA:
     ; ports 0x1f0 - 0x1f7 for primary ATA harddisk controller
-    mov ebx, eax
-
-    mov edx, 0x1f6      ; LBA 24 - 27 bits port
-    shr eax, 24
-    or al, 11100000b    ; for LBA mode
-    out dx, al
-
-    mov edx, 0x1f2      ; sector count port
-    mov al, cl
-    out dx, al
-
     mov edx, 0x1f3      ; LBA 0 - 7 bits port
-    mov al, bl
     out dx, al
 
     mov edx, 0x1f4      ; LBA 8 - 15 bits port
-    mov eax, ebx
     shr eax, 8
     out dx, al
 
     mov edx, 0x1f5      ; LBA 16 - 23 bits port
-    mov eax, ebx
-    shr eax, 16
+    shr eax, 8
     out dx, al
 
-    mov edx, 0x1f7      ; command port
+    mov edx, 0x1f6      ; LBA 24 - 27 bits port
+    shr eax, 8
+    or al, 11100000b    ; bit 4 for master, bit 6 for LBA mode, bit 7 and 5 for old ATA drives (backward compatibility)
+    out dx, al
+
+    mov edx, 0x1f2      ; sector count port
+    mov al, bl
+    out dx, al
+
+    mov edx, 0x1f7      ; command/status port
     mov al, 0x20        ; read with retry
     out dx, al
 
+    mov ecx, 4          ; 4 retries
 .wait:
     in al, dx
+    test al, 0x80
+    jne .retry          ; is BSY flag set
     test al, 8
-    jz .wait            ; wait until sector buffer is ready
-
-    mov eax, 512 / 2
-    xor bx, bx
-    mov bl, cl
-    mul bx              ; sectors in words
-
-    mov ecx, eax
+    jne .recv           ; is sector buffer ready
+.retry:
+    dec ecx
+    cmp ecx, 0
+    jg .wait
+.recv:
+    in al, dx
+    test al, 0x80
+    jne .recv
+    test al, 0x21
+    jne .err
+.ready:
+    mov rcx, BYTES_PER_SECTOR / 2
     mov rdx, 0x1f0      ; ATA data port
     rep insw
 
+    mov edx, 0x1f7      ; status port
+    in al, dx           ; wait 400ns
+    in al, dx
+    in al, dx
+    in al, dx
+
+    dec bl
+    cmp bl, 0           
+    jg .recv            ; read next sector
+    ret
+.err:
+    stc ; carry flag to indicate an error
+    ret
+
+; eax = cluster number
+; rdi = dest addr
+; output: loads clusters to dest addr (rdi increases)
+readClusterChain:
+    movzx ebx, byte [SectorsPerCluster]
+    mul ebx
+    add eax, root_dir_lba
+    call readATA
+    ; TODO: get next cluster from FAT
+    ; TODO: if < 0xfff8 repeat
+    ret
+
+read_root_dir:
+    mov bl, MAX_ROOT_ENTRIES * 32 / BYTES_PER_SECTOR
+    mov eax, root_dir_lba
+    mov rdi, root_dir_addr
+    call readATA 
+    ret
+
+read_fat:
+    mov bl, SECTORS_PER_FAT
+    mov eax, fat_lba
+    mov rdi, fat_addr
+    call readATA
     ret
 
 load_kernel:
-    ; TODO: read FAT
-
+    call read_fat
     call read_root_dir
 
     movzx eax, word [root_dir_addr+start_cluster]       ; stores start_cluster-2 (because first 2 are reserved)
     add eax, 2
 
     mov rdi, kernel_addr
-    call readClusters
-    ret
-
-.at_cluster_msg: db "start cluster of file: ", 0
-.file_size_msg: db "file size (in bytes): ", 0
-
-read_root_dir:
-    mov cl, 1
-    mov eax, root_dir_lba
-    mov rdi, root_dir_addr
-    call readSectors64
-    ret
-
-; input: eax = cluster number
-; loads cluster to addr in rdi and rdi gets increased
-readClusters:
-    movzx ebx, byte [SectorsPerCluster]
-    mov cl, bl
-    mul ebx
-    add eax, root_dir_lba
-    call readSectors64
-    ; TODO: get next cluster from FAT
-    ; TODO: if < 0xfff8 repeat
+    call readClusterChain
     ret
